89fb296fb7e656c7c98aa6d55fee25f42cd3c853
Fatema Jannat Ema <fatemajannatemamu@gmail.com>
2026-06-09 10:27:28 +0600

    Add ContestStats class to manage contest scores

--- Task 2/YouDo1.java
+++ Task 2/YouDo1.java
@@ -1,1 +1,15 @@
+class ContestStats {
+    int[] scores = {85, 90, 95};
+
+    void printFirstScore() {
+        System.out.println(scores[0]);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        ContestStats stats = new ContestStats();
+        stats.printFirstScore();
+    }
+}
 
