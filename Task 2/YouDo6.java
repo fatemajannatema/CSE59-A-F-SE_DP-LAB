2ef3d9688b74586fa826102dd8c2a70648e79984
Fatema Jannat Ema <fatemajannatemamu@gmail.com>
2026-06-09 11:05:08 +0600

    Add Applet and GameApplet classes with main method

--- Task 2/YouDo6.java
+++ Task 2/YouDo6.java
@@ -1,1 +1,19 @@
+class Applet {
+    void launch() {
+        System.out.println("Applet launched");
+    }
+}
+class GameApplet extends Applet {
+    void play() {
+        System.out.println("Playing game");
+    }
+}
 
+class Main {
+    public static void main(String[] args) {
+         GameApplet gameApplet = new GameApplet();
+         gameApplet.launch();
+         gameApplet.play();
+         
+    }
+}
