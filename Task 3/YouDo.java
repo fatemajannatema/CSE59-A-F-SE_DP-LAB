a5b07a3c50384864d79807ba990abce554795b43
Fatema Jannat Ema <fatemajannatemamu@gmail.com>
2026-06-16 09:59:13 +0600

    Update YouDo.java

--- Task 3/YouDo.java
+++ Task 3/YouDo.java
@@ -1,1 +1,39 @@
+public class User{
+    private String username;
+    private String email;
+    
+    public User(String username,String email){
+        this.username = username;
+        this.email= email;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+}
 
+
+class EmailValidator{
+    
+    public static boolean isValid(String email){
+        return email != null && email.contains("@");
+    }
+}
+
+
+class UserRepository{
+    
+    public void save(User user) {
+        if (EmailValidator.isValid(user.getEmail())){
+            
+            System.out.println("connecting to database...");
+            System.out.println("Saving user"+user.getUsername() +"to the users table.");
+        }else{
+            System.out.println("Invalid email format.");
+        }
+    }
+}
