aa28659b99fdc719ddbf2e2a31edc676c18b3a4b
Fatema Jannat Ema <fatemajannatemamu@gmail.com>
2026-06-23 10:34:48 +0600

    Add TeamMember, Employee, and Contractor classes

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,53 @@
+abstract class TeamMember {
+    protected String name;
+    
+    public TeamMember(String name) {
+        
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+}
 
+interface BonusEligible {
+    
+    void calculateBonus();
+}
+
+class Employee extends TeamMember implements BonusEligible {
+    
+    public Employee(String name) {
+        
+        super(name);
+    }
+    
+    @Override
+    
+    public void calculateBonus() {
+        
+        System.out.println("Calculating standard employee bonus...");
+    }
+}
+
+class Constractor extends TeamMember {
+    
+    public Constractor(String name) {
+        
+        super(name);
+    }
+    
+    public void submitInvoice() {
+        
+        System.out.println("Submitting constractor invoice...");
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee employee = new Employee("John");
+        employee.calculateBonus();
+    }
+}
