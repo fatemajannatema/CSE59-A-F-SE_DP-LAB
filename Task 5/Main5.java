b0a78e9e515d8e947df7fa237f319b5f435da891
Fatema Jannat Ema <fatemajannatemamu@gmail.com>
2026-06-30 11:24:20 +0600

    Implement university member classes and research interface
    Added classes for university members, including undergraduate students, graduate research assistants (GRA), and professors, along with their respective methods for attending lectures and conducting research.

--- Task 5/Main5.java
+++ Task 5/Main5.java
@@ -1,1 +1,52 @@
+class UniversityMember {
+
+    public void attendLecture() {
+        System.out.println("Attending a lecture.");
+    }
+}
+
+interface Researcher {
+    void conductLabResearch();
+}
+
+
+class UndergraduateStudent extends UniversityMember {
+    
+}
+
+
+class GRA extends UniversityMember implements Researcher {
+
+    @Override
+    public void conductLabResearch() {
+        System.out.println("GRA: Assisting in experiments and research work.");
+    }
+}
+
+class Professor extends UniversityMember implements Researcher {
+
+    @Override
+    public void conductLabResearch() {
+        System.out.println("Professor: Publishing papers and leading research.");
+    }
+}
+
+
+public class Main5 {
+    public static void main(String[] args) {
+
+        UniversityMember student = new UndergraduateStudent();
+        student.attendLecture();
+
+        System.out.println();
+
+        Researcher gra = new GRA();
+        gra.conductLabResearch();
+
+        System.out.println();
+
+        Researcher prof = new Professor();
+        prof.conductLabResearch();
+    }
+}
 
